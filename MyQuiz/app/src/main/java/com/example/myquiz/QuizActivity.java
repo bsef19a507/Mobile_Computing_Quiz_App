385d9370cc37b9c33754edd46c37ca52e33ad626
redmoon <bsef19a507@pucit.edu.pk>
2022-05-24 01:45:04 +0500

    Sigle Activity for Multiple Screens

--- MyQuiz/app/src/main/java/com/example/myquiz/QuizActivity.java
+++ MyQuiz/app/src/main/java/com/example/myquiz/QuizActivity.java
@@ -2,9 +2,12 @@ package com.example.myquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
@@ -13,7 +16,28 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        Button btn_nxt = findViewById(R.id.next_btn);
+
+        btn_nxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextQuestion();
+            }
+        });
     }
+
+    private void nextQuestion(){
+        Intent intent = new Intent(QuizActivity.this, QuizActivity.class);
+        startActivity(intent);
+
+        TextView question = findViewById(R.id.question);
+        question.setText("Q.2: After how many years Pakistan got its first constitution?");
+    }
+
+
+
+
 
     public void onRadioButtonClicked(View view){
         // Is the button now checked?
