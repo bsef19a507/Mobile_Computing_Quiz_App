cfc4929862b3ad427d8fa8ef132f38efa9b7227d
redmoon <bsef19a507@pucit.edu.pk>
2022-05-27 02:10:38 +0500

    Score Board activity added to QuizApp

--- MyQuiz/app/src/main/java/com/example/myquiz/SubmitActivity.java
+++ MyQuiz/app/src/main/java/com/example/myquiz/SubmitActivity.java
@@ -3,6 +3,7 @@ package com.example.myquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SubmitActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class SubmitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
+
+        TextView result_view = (TextView)findViewById(R.id.score);
+        Bundle bundle = getIntent().getExtras();
+
+        if(bundle.getString("result")!= null)
+        {
+           String score = bundle.getString("result");
+           result_view.setText(score);
+        }
     }
 }
